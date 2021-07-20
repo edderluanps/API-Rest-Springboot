a1835baa5416588d9c700ceeff6cab583b161a77
Edder Luan <eluann100@gmail.com>
2021-07-20 20:37:54 -0300

    User + UserRepositories

---
+++ src/main/java/com/project/apispringtest/domain/User.java
@@ -0,0 +1,61 @@
+package com.project.apispringtest.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    private String id;
+    private String name;
+    private String email;
+
+    public User() {
+    }
+
+    public User(String id, String name, String email) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 41 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final User other = (User) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+    
+}
