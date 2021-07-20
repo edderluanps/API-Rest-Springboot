d443c6bee096f0262dfbec524f27ed774f189653
Edder Luan <eluann100@gmail.com>
2021-07-20 15:46:47 -0300

    first commit

---
+++ src/main/java/com/project/apispringtest/ApispringtestApplication.java
@@ -0,0 +1,13 @@
+package com.project.apispringtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApispringtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApispringtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/apispringtest/ApispringtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.apispringtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApispringtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
