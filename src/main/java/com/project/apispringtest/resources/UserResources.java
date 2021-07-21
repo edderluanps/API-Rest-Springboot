7c612633eb9c62979d8e97b40cf7e632b869a774
Edder Luan <eluann100@gmail.com>
2021-07-21 17:11:13 -0300

    Using DTO

--- src/main/java/com/project/apispringtest/resources/UserResources.java
+++ src/main/java/com/project/apispringtest/resources/UserResources.java
@@ -1,8 +1,10 @@
 package com.project.apispringtest.resources;
 
 import com.project.apispringtest.domain.User;
+import com.project.apispringtest.dto.UserDTO;
 import com.project.apispringtest.services.UserServices;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,10 +19,11 @@ public class UserResources {
     private UserServices uservices;
     
     @RequestMapping(method=RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<UserDTO>> findAll(){
  
         List<User> list = uservices.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listdto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listdto);
         
     }
     
